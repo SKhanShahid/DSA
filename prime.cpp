8df94ba05a4d0e5630937acd248351dc9fc76926
shahid s khan <skhanshahid84@gmail.com>
2025-05-07 20:59:08 +0530

    added indentation

--- prime.cpp
+++ prime.cpp
@@ -1,26 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     int n;
-    cout << "Enter a number: "<< endl;
+    cout << "Enter a number: " << endl;
     cin >> n;
     bool isPrime = true;
-    for(int i = 2; i < n ; i++)
-    {   //rem=0, not a prime
+    for (int i = 2; i < n; i++)
+    { // rem=0, not a prime
         if (n % i == 0)
         {
             isPrime = false;
             break;
         }
     }
-    if (isPrime==0)
+    if (isPrime == 0)
     {
         cout << n << " is not a prime number." << endl;
     }
-    else 
+    else
     {
         cout << n << " is a prime number." << endl;
     }
-   
-    }
+}
