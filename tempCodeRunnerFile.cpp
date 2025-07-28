398bfdf3ac3a0bd6cec2a0532f966ecc5e0f235d
shahid s khan <skhanshahid84@gmail.com>
2025-07-29 01:12:56 +0530

    Initial commit

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,10 +1,10 @@
-#incliude<iostream>
+#include<iostream>
 using namespace std;
 int main(){
     int a=4;
     int b=5;
-    cout<<"a&b="<<a&b<<endl; // bitwise AND
-    cout<<"a|b="<<a|b<<endl; // bitwise OR
-    cout<<"a^b="<<a^b<<endl; // bitwise XOR
-    cout<<"~a="<<~a<<endl; // bitwise NOT
+    cout<<"a&b="<<(a&b)<<endl; // bitwise AND
+    cout<<"a|b="<<(a|b)<<endl; // bitwise OR
+    cout<<"a^b="<<(a^b)<<endl; // bitwise XOR
+    cout<<"~a="<<(~a)<<endl; // bitwise NOT
 }
